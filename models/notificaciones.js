b8f792392cb4d4bcd3ed5f2213cf3daa04095f09
Nacho <a01644423@tec.mx>
2025-04-11 12:54:04 -0600

    updated the notifications db model

--- models/notificaciones.js
+++ models/notificaciones.js
@@ -8,4 +8,8 @@ const getNoficicacionesByEscuela = async (escuela) => {
     return await db.query('SELECT n.* FROM notificaciones n JOIN notificaciones_a_administradores_de_escuelas naade ON naade.id_notificacion = n.id_notificacion WHERE naade.usuario_escuela = $1', [escuela]);
 }
 
-module.exports = { getNoficicacionesByAliado, getNoficicacionesByEscuela }
+const getNoficicacionesByAdmin = async (admin) => {
+    return await db.query('SELECT n.* FROM notificaciones n JOIN notificaciones_a_administradores_de_escuelas naade ON naade.id_notificacion = n.id_notificacion WHERE naade.usuario_admin = $1 OR naade.usuario_admin = $2', [admin, 'superadmin']);
+}
+
+module.exports = { getNoficicacionesByAliado, getNoficicacionesByEscuela, getNoficicacionesByAdmin }
