a82cc8d3c0a8aad95352d3df47f903b62aaf19ac
Nacho <a01644423@tec.mx>
2025-04-11 12:43:55 -0600

    updated the ally db model

--- models/aliados.js
+++ models/aliados.js
@@ -12,4 +12,4 @@ const getApoyosByAliado = async (aliado) => {
     return await db.query('SELECT DISTINCT tipo_apoyo FROM tipos_de_apoyo_a_brindar WHERE usuario_aliado = $1', [aliado]);
 }
 
-module.exports = { getAllAliados, getApoyosByAliado }
+module.exports = { getAllAliados, getAllActiveAliados, getApoyosByAliado }
