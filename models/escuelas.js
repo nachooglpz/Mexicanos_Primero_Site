d9f2e380f6a0631168922e56e3a2a2b00080c6d1
Nacho <a01644423@tec.mx>
2025-04-11 12:31:05 -0600

    updated the school db model

---
+++ models/escuelas.js
@@ -0,0 +1,11 @@
+const db = require('../db.js');
+
+const getAllEscuelas = async () => {
+    return await db.query('SELECT * FROM administradores_de_escuela WHERE estatus_activo = true');
+}
+
+const getNecesidadesByEscuela = async (aliado) => {
+    return await db.query('SELECT DISTINCT necesidad FROM necesidades WHERE usuario_aliado = $1', [aliado]);
+}
+
+module.exports = { getAllEscuelas, getNecesidadesByEscuela }
