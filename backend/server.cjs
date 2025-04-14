a78dc50f6ef4eb09ef92d939da08ccba816537f2
Nacho <a01644423@tec.mx>
2025-04-14 13:46:53 -0600

    implemented react on the escuelas page

--- backend/server.cjs
+++ backend/server.cjs
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express();
 
-const { aliadosRouter } = require('./routes/aliados.cjs')
+const { aliadosRouter } = require('./routes/aliados.cjs');
+const { escuelasRouter } = require('./routes/escuelas.cjs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ app.use(express.static('public'));
 
 // Routes go here
 app.use('/api/aliados', aliadosRouter);
+app.use('/api/escuelas', escuelasRouter);
 
 // Start the server listening on PORT
 app.listen(PORT, () => {
