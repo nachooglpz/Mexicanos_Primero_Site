d15b679f269073e0e11f58e48f055d7c3aceac4b
Nacho <a01644423@tec.mx>
2025-04-28 18:18:49 -0600

    added register for both schools and allies

--- server.cjs
+++ server.cjs
@@ -8,8 +8,7 @@ const { escuelasRouter } = require('./Usuarios/escuelas_route.cjs');
 const { adminRouter } = require('./Usuarios/admin_route.cjs');
 const { notificacionesRouter } = require('./Comunicacion/notificaciones_route.cjs');
 const documentosRouter = require('./Usuarios/documentos_route.cjs');
-
-const inicioSesionModel = require('./Usuarios/inicioSesion_model.cjs');
+const { sesionRouter } = require('./Usuarios/sesion_registro_route.cjs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -26,6 +25,7 @@ app.use('/api/escuelas', escuelasRouter);
 app.use('/api/notificaciones', notificacionesRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/documentos', documentosRouter);
+app.use('/api/sesion', sesionRouter);
 
 //ruta para obtener todos los usuarios
 app.get('/api/usuarios', async (req, res) => {
@@ -56,22 +56,6 @@ app.get('/api/usuarios', async (req, res) => {
         console.error('Error al obtener usuarios:', error);
         res.status(500).send('Error al obtener usuarios');
     }
-});
-
-// Login request
-const validateLoginQuery = (req, res, next) => {
-    if (!('usuario' in req.query && 'contrasena' in req.query)) {
-        const error = new Error(`Query is missing usuario or contrasena. Arguments: ${req.query}`);
-        error.status = 401;
-        return next(error);
-    }
-    next();
-};
-
-app.get('/api/login', validateLoginQuery, async (req, res, next) => {
-    const { usuario, contrasena } = req.query;
-    const userInfo = await inicioSesionModel.getExistentUser(usuario, contrasena);
-    res.send(userInfo);
 });
 
 app.use((err, req, res, next) => {
